0c5fb430445f281c62eb3b1500585d01cced874f
Kota Yoshitsugu <k.yoshitsugu@mmj.ne.jp>
2016-03-05 13:19:49 +0900

    Add source

---
+++ about.md
@@ -0,0 +1,12 @@
+---
+title: About Me
+---
+Web Engineer in Tokyo, Japan.  
+  
+Ruby / Ruby on Rails  
+Now I'm interested in Haskell  
+
+<i class="fa fa-github fa-fw"></i>:  <a href="https://github.com/yoshitsugu" target="_blank">yoshitsugu</a>  
+<i class="fa fa-twitter fa-fw"></i>:  <a href="https://twitter.com/_yoshitsugu" target="_blank">@_yoshitsugu</a>  
+Qiita:  <a href="http://qiita.com/yoshitsugu" target="_blank">yoshitsugu</a>
+
---
+++ site.hs
@@ -0,0 +1,99 @@
+--------------------------------------------------------------------------------
+{-# LANGUAGE OverloadedStrings #-}
+import           Data.Monoid            ((<>))
+import           Hakyll
+import qualified Text.Highlighting.Kate as K
+
+--------------------------------------------------------------------------------
+main :: IO ()
+main = hakyll $ do
+    match "images/*" $ do
+        route   idRoute
+        compile copyFileCompiler
+
+    match "css/*.scss" $ do
+        route $ setExtension "css"
+        compile $ getResourceString
+          >>= withItemBody (unixFilter "sass" ["-s", "--scss"])
+          >>= return . fmap compressCss
+
+    match "css/*.css" $ do
+        route   idRoute
+        compile compressCssCompiler
+
+    create ["css/highlight.css"] $ do
+      route   idRoute
+      compile $ makeItem (compressCss $ K.styleToCss K.haddock)
+
+    match (fromList ["about.md"]) $ do
+        route   $ setExtension "html"
+        compile $ pandocCompiler
+            >>= loadAndApplyTemplate "templates/default.html" defaultContext
+            >>= relativizeUrls
+
+    tags <- buildTags "posts/*" (fromCapture "tags/*.html")
+
+
+    tagsRules tags $ \tag patt -> do
+        route   idRoute
+        compile $ do
+            posts <- recentFirst =<< loadAll patt
+            let tagCtx = constField "title" ("Posts tagged " ++ tag)
+                      <> listField "posts" (postCtxWithTags tags) (return posts)
+                      <> defaultContext
+
+            makeItem ""
+                >>= loadAndApplyTemplate "templates/tag.html" tagCtx
+                >>= loadAndApplyTemplate "templates/default.html" tagCtx
+                >>= relativizeUrls
+
+
+    match "posts/*" $ do
+        route $ setExtension "html"
+        compile $ pandocCompiler
+            >>= saveSnapshot "content"
+            >>= loadAndApplyTemplate "templates/post.html"    (postCtxWithTags tags)
+            >>= loadAndApplyTemplate "templates/default.html" (postCtxWithTags tags)
+            >>= relativizeUrls
+
+    create ["archive.html"] $ do
+        route idRoute
+        compile $ do
+            posts <- recentFirst =<< loadAll "posts/*"
+            let archiveCtx =
+                    listField "posts" postCtx (return posts) `mappend`
+                    constField "title" "Archives"            `mappend`
+                    defaultContext
+
+            makeItem ""
+                >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
+                >>= loadAndApplyTemplate "templates/default.html" archiveCtx
+                >>= relativizeUrls
+
+    match "index.html" $ do
+        route   idRoute
+        compile $ do
+            posts <- recentFirst =<< loadAllSnapshots "posts/*" "content"
+            let teaserCtx = teaserField "teaser" "content" `mappend` postCtx
+                indexCtx =
+                    listField "posts" teaserCtx (return $ take 5 posts) `mappend`
+                    defaultContext
+
+            getResourceBody
+                >>= applyAsTemplate indexCtx
+                >>= loadAndApplyTemplate "templates/default.html" indexCtx
+                >>= relativizeUrls
+
+
+    match "templates/*" $ compile templateCompiler
+
+
+--------------------------------------------------------------------------------
+postCtx :: Context String
+postCtx =
+    dateField "date" "%B %e, %Y" `mappend`
+    defaultContext
+
+postCtxWithTags :: Tags -> Context String
+postCtxWithTags tags = tagsField "tags" tags `mappend` postCtx
+
