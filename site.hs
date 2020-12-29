0560b0a0a6e6dca95c0149f7a49a0995ecb93ece
yoshitsugu <yoshitsugu@users.noreply.github.com>
2020-12-29 19:54:09 +0900

    Add dark theme

--- site.hs
+++ site.hs
@@ -1,175 +1,180 @@
 {-# LANGUAGE OverloadedStrings #-}
-import           Data.Monoid            ((<>))
-import           Hakyll
-import           Text.Pandoc.Options
-import Text.Pandoc.Highlighting
+
+import Data.Monoid ((<>))
+import Hakyll
 import qualified Skylighting.Format.HTML as SF
 import qualified Skylighting.Styles as SS
+import Text.Pandoc.Highlighting
+import Text.Pandoc.Options
 
 hostName :: String
 hostName = "https://yoshitsugu.net"
 
-
 main :: IO ()
 main = hakyll $ do
-    match "images/**/*" $ do
-        route   idRoute
-        compile copyFileCompiler
+  match "images/**/*" $ do
+    route idRoute
+    compile copyFileCompiler
 
-    match "images/*" $ do
-        route   idRoute
-        compile copyFileCompiler
+  match "images/*" $ do
+    route idRoute
+    compile copyFileCompiler
 
-    match (fromList ["google9af023e4743ca32a.html", "robots.txt"]) $ do
-        route   idRoute
-        compile copyFileCompiler
+  match (fromList ["google9af023e4743ca32a.html", "robots.txt"]) $ do
+    route idRoute
+    compile copyFileCompiler
 
-    match "css/*.scss" $ do
-        route $ setExtension "css"
-        compile $ getResourceString
-          >>= withItemBody (unixFilter "sass" ["--stdin"])
-          >>= return . fmap compressCss
+  match "css/*.scss" $ do
+    route $ setExtension "css"
+    compile $
+      getResourceString
+        >>= withItemBody (unixFilter "sass" ["--stdin"])
+        >>= return . fmap compressCss
 
-    match "css/*.css" $ do
-        route   idRoute
-        compile compressCssCompiler
+  create ["css/highlight.css"] $ do
+    route idRoute
+    compile $ makeItem $ (compressCss $ SF.styleToCss SS.haddock) ++ "@media (prefers-color-scheme: dark) {" ++ (compressCss $ SF.styleToCss SS.breezeDark) ++ "}"
 
-    match "js/*.js" $ do
-        route   idRoute
-        compile copyFileCompiler
+  match "css/*.css" $ do
+    route idRoute
+    compile compressCssCompiler
 
-    create ["css/highlight.css"] $ do
-      route   idRoute
-      compile $ makeItem (compressCss $ SF.styleToCss SS.haddock)
+  match "js/*.js" $ do
+    route idRoute
+    compile copyFileCompiler
 
-    match (fromList ["about.md"]) $ do
-        route   $ setExtension "html"
-        compile $ customPandocCompiler
-            >>= loadAndApplyTemplate "templates/default.html" defaultCtx
-            >>= relativizeUrls
+  match (fromList ["about.md"]) $ do
+    route $ setExtension "html"
+    compile $
+      customPandocCompiler
+        >>= loadAndApplyTemplate "templates/default.html" defaultCtx
+        >>= relativizeUrls
 
-    tags <- buildTags "posts/*" (fromCapture "tags/*.html")
+  tags <- buildTags "posts/*" (fromCapture "tags/*.html")
 
+  tagsRules tags $ \tag patt -> do
+    route idRoute
+    compile $ do
+      posts <- recentFirst =<< loadAll patt
+      let tagCtx =
+            constField "title" ("Posts tagged " ++ tag)
+              <> listField "posts" (postCtxWithTags tags) (return posts)
+              <> defaultCtx
 
-    tagsRules tags $ \tag patt -> do
-        route   idRoute
-        compile $ do
-            posts <- recentFirst =<< loadAll patt
-            let tagCtx = constField "title" ("Posts tagged " ++ tag)
-                      <> listField "posts" (postCtxWithTags tags) (return posts)
-                      <> defaultCtx
+      makeItem ""
+        >>= loadAndApplyTemplate "templates/tag.html" tagCtx
+        >>= loadAndApplyTemplate "templates/default.html" tagCtx
+        >>= relativizeUrls
 
-            makeItem ""
-                >>= loadAndApplyTemplate "templates/tag.html" tagCtx
-                >>= loadAndApplyTemplate "templates/default.html" tagCtx
-                >>= relativizeUrls
+  match "posts/*" $ do
+    route $ setExtension "html"
+    compile $
+      customPandocCompiler
+        >>= saveSnapshot "content"
+        >>= loadAndApplyTemplate "templates/post.html" (postCtxWithTags tags)
+        >>= loadAndApplyTemplate "templates/default.html" (postCtxWithTags tags)
+        >>= relativizeUrls
 
+  create ["archive.html"] $ do
+    route idRoute
+    compile $ do
+      posts <- recentFirst =<< loadAll "posts/*"
+      let archiveCtx =
+            listField "posts" postCtx (return posts)
+              `mappend` constField "title" "Archives"
+              `mappend` defaultCtx
 
-    match "posts/*" $ do
-        route $ setExtension "html"
-        compile $ customPandocCompiler
-            >>= saveSnapshot "content"
-            >>= loadAndApplyTemplate "templates/post.html"    (postCtxWithTags tags)
-            >>= loadAndApplyTemplate "templates/default.html" (postCtxWithTags tags)
-            >>= relativizeUrls
+      makeItem ""
+        >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
+        >>= loadAndApplyTemplate "templates/default.html" archiveCtx
+        >>= relativizeUrls
 
-    create ["archive.html"] $ do
-        route idRoute
-        compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
-            let archiveCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Archives"            `mappend`
-                    defaultCtx
+  match "index.html" $ do
+    route idRoute
+    compile $ do
+      posts <- recentFirst =<< loadAllSnapshots "posts/*" "content"
+      let teaserCtx = teaserField "teaser" "content" `mappend` postCtx
+          indexCtx =
+            listField "posts" teaserCtx (return $ take 10 posts)
+              `mappend` defaultCtx
 
-            makeItem ""
-                >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
-                >>= loadAndApplyTemplate "templates/default.html" archiveCtx
-                >>= relativizeUrls
+      getResourceBody
+        >>= applyAsTemplate indexCtx
+        >>= loadAndApplyTemplate "templates/default.html" indexCtx
+        >>= relativizeUrls
 
-    match "index.html" $ do
-        route   idRoute
-        compile $ do
-            posts <- recentFirst =<< loadAllSnapshots "posts/*" "content"
-            let teaserCtx = teaserField "teaser" "content" `mappend` postCtx
-                indexCtx =
-                    listField "posts" teaserCtx (return $ take 10 posts) `mappend`
-                    defaultCtx
+  create ["sitemap.xml"] $ do
+    route idRoute
+    compile $ do
+      posts <- recentFirst =<< loadAll "posts/*"
+      pages <- loadAll $ fromList ["index.html", "about.md"]
+      let allPosts = (return (pages ++ posts))
+      let sitemapCtx =
+            mconcat
+              [ listField "entries" pageCtx allPosts,
+                constField "host" hostName,
+                defaultContext
+              ]
+      makeItem ""
+        >>= loadAndApplyTemplate "templates/sitemap.xml" sitemapCtx
+        >>= cleanIndexHtmls
 
-            getResourceBody
-                >>= applyAsTemplate indexCtx
-                >>= loadAndApplyTemplate "templates/default.html" indexCtx
-                >>= relativizeUrls
-
-    create ["sitemap.xml"] $ do
-         route   idRoute
-         compile $ do
-           posts <- recentFirst =<< loadAll "posts/*"
-           pages <- loadAll $ fromList ["index.html", "about.md"]
-           let allPosts = (return (pages ++ posts))
-           let sitemapCtx = mconcat
-                            [ listField "entries" pageCtx allPosts
-                            , constField "host" hostName
-                            , defaultContext
-                            ]
-           makeItem ""
-            >>= loadAndApplyTemplate "templates/sitemap.xml" sitemapCtx
-            >>= cleanIndexHtmls
-
-    match "templates/*" $ compile templateCompiler
-
+  match "templates/*" $ compile templateCompiler
 
 --------------------------------------------------------------------------------
 customPandocCompiler :: Compiler (Item String)
 customPandocCompiler =
-  let writerOptions = defaultHakyllWriterOptions {
-        writerHighlightStyle = Just zenburn
-        }
-  in pandocCompilerWith defaultHakyllReaderOptions writerOptions
-
+  let writerOptions =
+        defaultHakyllWriterOptions
+          { writerHighlightStyle = Just zenburn
+          }
+   in pandocCompilerWith defaultHakyllReaderOptions writerOptions
 
 defaultCtx :: Context String
 defaultCtx =
-    urlField "ogpurl" `mappend`
-    constField "host" hostName `mappend`
-    defaultContext
+  urlField "ogpurl"
+    `mappend` constField "host" hostName
+    `mappend` defaultContext
 
 postCtx :: Context String
 postCtx =
-    dateField "date" "%B %e, %Y" `mappend`
-    descriptionField "description" "content" `mappend`
-    defaultCtx
+  dateField "date" "%B %e, %Y"
+    `mappend` descriptionField "description" "content"
+    `mappend` defaultCtx
 
 postCtxWithTags :: Tags -> Context String
 postCtxWithTags tags = tagsField "tags" tags `mappend` postCtx
 
 pageCtx :: Context String
-pageCtx = mconcat
-    [ modificationTimeField "mtime" "%U"
-    , modificationTimeField "lastmod" "%Y-%m-%d"
-    , dateField "updated" "%Y-%m-%dT%H:%M:%SZ"
-    , constField "host" hostName
-    , dateField "date" "%B %e, %Y"
-    , defaultCtx
+pageCtx =
+  mconcat
+    [ modificationTimeField "mtime" "%U",
+      modificationTimeField "lastmod" "%Y-%m-%d",
+      dateField "updated" "%Y-%m-%dT%H:%M:%SZ",
+      constField "host" hostName,
+      dateField "date" "%B %e, %Y",
+      defaultCtx
     ]
 
 cleanIndexHtmls :: Item String -> Compiler (Item String)
 cleanIndexHtmls = return . fmap (replaceAll pattern replacement)
-    where
-      pattern = "/index.html"
-      replacement = const "/"
+  where
+    pattern = "/index.html"
+    replacement = const "/"
 
 descriptionField :: String -> Snapshot -> Context String
 descriptionField = teaserFieldWithSeparatorStripped "<!--more-->"
 
-teaserFieldWithSeparatorStripped :: String
-                         -> String
-                         -> Snapshot
-                         -> Context String
+teaserFieldWithSeparatorStripped ::
+  String ->
+  String ->
+  Snapshot ->
+  Context String
 teaserFieldWithSeparatorStripped separator key snapshot = field key $ \item -> do
-    body <- itemBody <$> loadSnapshot (itemIdentifier item) snapshot
-    case needlePrefix separator body of
-        Nothing -> fail $
-            "Hakyll.Web.Template.Context: no teaser defined for " ++
-            show (itemIdentifier item)
-        Just t -> return $ stripTags t
+  body <- itemBody <$> loadSnapshot (itemIdentifier item) snapshot
+  case needlePrefix separator body of
+    Nothing ->
+      fail $
+        "Hakyll.Web.Template.Context: no teaser defined for "
+          ++ show (itemIdentifier item)
+    Just t -> return $ stripTags t
